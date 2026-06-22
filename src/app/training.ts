4fc0902aa8d9aeb74f45156c1d26bceb64f939d9
Parviz Shirinov <parveeezy@gmail.com>
2026-06-22 11:02:19 +0500

    Homework-14: Complete tasks

---
+++ src/app/training.ts
@@ -0,0 +1,72 @@
+type IStatus = 'success' | 'error' | 'loading';
+type ITextFormat = 'uppercase' | 'capitalize' | 'lowercase';
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  studio?: 'DC' | 'Marvel' | 'WD' | 'WB';
+}
+
+interface IEmployee extends IUser {
+  department: string;
+  position: string;
+}
+
+let status: IStatus;
+let textFormat: ITextFormat;
+
+export const sum = (a: number, b: number): number => {
+  return a + b;
+};
+
+export const formatString = (str: string, variant: ITextFormat): string => {
+  let newStr = str;
+
+  if (variant === 'capitalize') {
+    newStr = newStr[0].toUpperCase() + newStr.slice(1);
+  } else if (variant === 'uppercase') {
+    newStr = newStr.toUpperCase();
+  } else if (variant === 'lowercase') {
+    newStr = newStr.toLowerCase();
+  }
+  return newStr;
+};
+
+export const filterString = (str: string, separator: string) => str.split(separator).join('');
+
+export const users: IUser[]= [
+  {
+    id: '1',
+    name: 'Dr. Strange',
+    email: 'strange@marvel.com',
+    phone: '0123456789',
+    studio: "Marvel",
+  },
+  {
+    id: '2',
+    name: 'Jack Sparrow',
+    email: 'sparrow-jack@google.com',
+    phone: '0123456789',
+    studio: 'WD',
+  },
+  {
+    id: '3',
+    name: 'Harry Potter',
+    email: 'callmepotter@hogwarts.mag',
+    phone: '0123456789',
+    studio: "WB",
+  },
+  {
+    id: '4',
+    name: 'Hulk',
+    email: 'hulkfrommarvel@mail.com',
+    phone: '0123456789',
+    studio: "Marvel",
+  }
+]
+
+export const filterUsers = (arr: IUser[], studio) => {
+  return arr.filter((el) => el.studio === studio);
+}
