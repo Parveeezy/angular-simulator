f015dcc2932e71c14beac05c9d5c6dc9ffbbecad
Parviz Shirinov <parveeezy@gmail.com>
2026-06-30 11:16:52 +0300

    Homework-15: fix collections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {HeaderComponent} from './layout/header/header.component';
-import {Color} from '../enums/Color';
-import {Collection} from '../collections';
+import { Component, OnInit } from '@angular/core';
+import { HeaderComponent } from './layout/header/header.component';
+import { Color } from '../enums/Color';
+import { Collection } from '../collections';
 
 @Component({
   selector: 'app-root',
@@ -21,13 +21,17 @@ export class AppComponent {
   checkColor(color: string) {
     return color === Color.BLUE || color === Color.GREEN || color === Color.RED;
   }
-  collection = new Collection([1,2,3,4,5,6]);
+  collection = new Collection([1, 2, 3, 4, 5, 6]);
   collection2 = new Collection([
-    {id: 1, name: 'Styopa', login: 'STEPAN'},
-    {id: 2, name: 'Petya', login: 'Petka'},
-    {id: 3, name: 'Vasya', login: 'Vasek'}
+    { id: 1, name: 'Styopa', login: 'STEPAN' },
+    { id: 2, name: 'Petya', login: 'Petka' },
+    { id: 3, name: 'Vasya', login: 'Vasek' }
   ]);
-  
+
+  savedData = localStorage.getItem('data-items')
+  data = this.savedData ? JSON.parse(this.savedData) : []
+  collection3 = new Collection(this.data);
+
   lastVisit: string | null = null;
 
   checkLastVisit() {
